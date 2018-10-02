9e8d8af34dbc39caa02249700690ebd8cbe8c608
salimtaiyo <ichigeki.ccCsc@hotmail.com>
2018-10-01 23:55:02 -0400

    added mongodb

--- server/app.js
+++ server/app.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const port = 5000;
 const cors = require('cors');
+const monk = require('monk');
+const rateLimit = require("express-rate-limit");
+
+const db = monk("localhost/tweet");
+const tweet = db.get('posts')
 
 app.use(cors());
 app.use(express.json());
@@ -16,19 +21,39 @@ function validation(post){
             post.content && post.content.toString().trim !== ''
 }
 
+const limiter = rateLimit({
+    windowMs: 30 * 1000, // 15 minutes
+    max: 3 // limit each IP to 100 requests per windowMs
+  });
+   
+  //  apply to all requests
+  app.use(limiter);
+
+
 app.post('/posts', (req,res) => {
     if(validation(req.body)){
         const post = {
             name: req.body.name.toString(),
-            content: req.body.content.toString()
+            content: req.body.content.toString(),
+            created: new Date()
         }
-        console.log(post);
+        tweet.insert(post)
+            .then(createdPost => {
+                res.json(createdPost)
+            })
     }else{
         res.status(422)
         res.json({
             message: 'Fill it up'
         })
     }
+})
+
+
+
+
+app.get('/posts', (req,res) => {
+tweet.find().then(tweet => res.json(tweet))
 })
 app.listen(port, () => {
     console.log('its running on port '+ port); 
